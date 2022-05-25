e625804e469ed0a36c2877791f362f3cf674d629
Alex Otero Ferro <aotero@idealista.com>
2022-05-25 14:11:29 +0200

    Updated layout

--- keyboards/splitkb/kyria/kyria.c
+++ keyboards/splitkb/kyria/kyria.c
@@ -55,9 +55,15 @@ bool oled_task_kb(void) {
         const int REFRESH_INTERVAL = 16; //in milliseconds
         const int SCREEN_HEIGHT = 64;
         int timer = 0;
-        char wpm_text[5];
+        char wpm_str[5];
         int height = 5;
         int currwpm = get_current_wpm();
+
+        //format current WPM value into a printable string
+        itoa(currwpm, wpm_str, 10);
+        oled_write_P(PSTR("WPM->"), false);
+        oled_write(wpm_str, false);
+
 
         //check if it's been long enough before refreshing graph
         if(timer_elapsed(timer) > REFRESH_INTERVAL){
@@ -72,22 +78,35 @@ bool oled_task_kb(void) {
             //timer refresh
             timer = timer_read();
         }
-
-        //format current WPM value into a printable string
-        itoa(currwpm, wpm_text, 10);
-        oled_set_cursor(14, 0);
-        oled_write("WPM: ", false);
-        oled_set_cursor(18, 0);
-        oled_write(wpm_text, false);
     } else {
-        static const char PROGMEM raw_logo[] = {
+        static const char PROGMEM idealista_logo[] = {
             0,  0,  0,  0,  0,  0,224,224,224,224,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,224,224,224,192,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,224,224,224,224,224,224,224,  0,  0,  0,  0,  0,  0,224,224,224,224,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,224,224,224,224,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
             0,  0,  0,240,240,240,241,241,241,241,  0,  0,  0,128,128,128,240,240,240,240,240,240,248,255,255,255,255,  0,  0,128,128,128,240,240,240,240,240,240,240,240,128,128,128,  0,  0,  0,  0,  0,  0,240,240,240,240,240,240,240,128,128,128,  0,  0,  0,  1,  1,  1,255,255,255,255,  0,  0,  0,240,240,240,241,241,241,241,  0,  0,  0,128,128,128,240,240,240,240,240,240,240,240,240,240,112,  0,  0,240,240,240,255,255,255,255,248,240,240,240,  0,  0,  0,  0,  0,112,240,240,240,240,240,240,240,128,128,128,  0,  0,  0,
             0,  0,  0,  0,  0,  0,255,255,255,255,  0,  0,  0,255,255,255,255,  0,  0,  0,  0,  0,  0,255,255,255,255,  0,  0,255,255,255,255, 60, 60, 60, 60, 60, 60, 63,127, 63, 63,  0,  0,128,192,192,192,252, 60, 60, 60, 60, 60,255,255,255,255,187,  0,  0,  0,  0,  0,255,255,255,255,  0,  0,  0,  0,  0,  0,255,255,255,255,  0,  0,  0,  7,  7,  7, 63, 60, 60, 60, 60, 60,124,224,192,192,192,  0,  0,  0,  0,  0,255,255,255,255,  0,  0,  0,  0,  0,  0,192,192,192,232,124, 60, 60, 60, 60, 60,255,255,255,255,  0,  0,  0,
             0,  0,  0,  0,  0,  0, 31, 31, 31, 31,  0,  0,  0,  3,  3,  3, 31, 30, 30, 30, 30, 30, 30, 31, 31, 31, 31,  0,  0,  1,  3,  3, 15, 30, 30, 30, 30, 30, 30, 30,  0,  0,  0,  0,  0,  1,  3,  3,  3, 31, 30, 30, 30, 30, 30, 31, 31, 31, 31, 15,  0,  0,  0,  0,  0, 31, 31, 31, 31,  0,  0,  0,  0,  0,  0, 31, 31, 31, 31,  0,  0,  0, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30,  3,  3,  3,  1,  0,  0,  0,  0,  0,  1,  3,  3,  3, 30, 30, 30, 30,  0,  0,  1,  3,  3, 11, 30, 30, 30, 30, 30, 30, 31, 31, 31, 31,  0,  0,  0,
         };
         oled_write_P(PSTR("\n\n"), false);
-        oled_write_raw_P(raw_logo, sizeof(raw_logo));
+        oled_write_raw_P(idealista_logo, sizeof(idealista_logo));
+    }
+    return false;
+}
+#endif
+
+
+#ifdef ENCODER_ENABLE
+bool encoder_update_kb(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
     }
     return false;
 }
